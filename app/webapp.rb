0e37fceadf9b3a3e8ca9b4695151de882bbf022a
Robby Ranshous <rranshous@gmail.com>
2023-08-21 17:52:40 -0400

    update readme; remove app ssl stuff

--- app/webapp.rb
+++ app/webapp.rb
@@ -5,19 +5,6 @@ configure do
   set :port, '4567'
 end
 
-configure :production do
-  certificate_content = File.open(ENV['SSL_CERT_PATH']).read
-  key_content = File.open(ENV['SSL_KEY_PATH']).read
-  puts "KEY CONTENT: #{key_content}"
-
-  set :server_settings, {
-    SSLEnable: true,
-    SSLCertificate: OpenSSL::X509::Certificate.new(certificate_content),
-    SSLPrivateKey: OpenSSL::PKey::RSA.new(key_content)
-  }
-end
-
-
 get '/' do
   puts "getting root"
   "hello w0rld"
